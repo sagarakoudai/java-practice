000d50f7315de25e480067363ac5cf0ef65849da
sagarakoudai <magnons180@gmail.com>
2021-03-23 23:46:21 +0900

    add skippr

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,17 +1,5 @@
-// $(function(){
-//   $('.box1').slideDown(function(){
-//     $('.box1').css({
-//       'background-color': '#0000ff',
-//       'height': '100px'
-//     }).slideUp();
-//   });
-// });
-
 $(function(){
-  $('.box1').on('click',function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+  $('button').on('click',function(){
+    $('ul').css('color','red');
   });
 });
