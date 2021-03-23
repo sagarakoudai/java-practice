2e3c2e544d1ea3008b6bc826d0122ad02ac5bcd6
sagarakoudai <magnons180@gmail.com>
2021-03-23 21:11:29 +0900

    exercise

--- app.js
+++ app.js
@@ -1,33 +1,112 @@
-// if文
-var orange = 100;
-var apple = 120;
+// // if文
+// var orange = 100;
+// var apple = 120;
 
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-}else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-}else{
-  alert('みかんの値段がりんごより高い');
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// }else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// }else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+
+// // while文
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+// // for文
+// var i;
+// var num = 0;
+
+// for(i = 1;i<11;i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+
+// 関数
+// var alertString;
+// alertString =addString("WebCamp");
+
+// alert(alertString);
+
+// function addString(strA){
+//   var addStr ="Hello " + strA;
+//   return addStr;
+// }
+
+
+// 複数の関数
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+while((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
 }
 
-// while文
-var max = 100;
-var num = 1;
-var count = 0;
+var js_hand = getJShand();
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
+var judge = winLose(user_hand,js_hand);
+
+if (user_hand != null){
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
 }
 
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+function getJShand(){
+  var js_hand_num = Math.floor(Math.random() * 3 );
+  var hand_name;
 
-// for文
-var i;
-var num = 0;
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if (js_hand_num == 2){
+    hand_name = "パー";
+  }
 
-for(i = 1;i<11;i++){
-  num = num + i; 
+  return hand_name;
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+function winLose(user,js){
+  var winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
